4e023e20b9080f10415c52b2af0b6180554417c7
Kelsea <kayk4sweet@hotmail.com>
2024-12-10 15:54:41 -0500

    Initial Commit

---
+++ Test for Github/Test_for_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_for_GithubApp.swift
+//  Test for Github
+//
+//  Created by Kelsea Donaldson on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_for_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test for GithubTests/Test_for_GithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  Test_for_GithubTests.swift
+//  Test for GithubTests
+//
+//  Created by Kelsea Donaldson on 12/10/24.
+//
+
+import Testing
+@testable import Test_for_Github
+
+struct Test_for_GithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
